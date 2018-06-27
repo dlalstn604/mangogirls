e5bace04a3e5194a5107054431641f2082a8f1aa
lms <to_6677@naver.com>
2018-06-27 15:19:46 +0900

    망고걸스

---
+++ blog/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.utils import timezone
+# Create your models here.
+
+class Soccer(models.Model):
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    text = models.TextField()
+    created_date = models.DateTimeField(
+        default=timezone.now)
+
+    def __str__(self):
+        return self.title
+    

4708e52f6a8507c5011c6aa7c72865d9ab54c649
lms <to_6677@naver.com>
2018-06-28 10:49:04 +0900

    댓글 클래스 만들기

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Soccer
+from .models import Soccer, Comment
 
 admin.site.register(Soccer)
+admin.site.register(Comment)
 # Register your models here.
--- blog/models.py
+++ blog/models.py
@@ -11,4 +11,17 @@ class Soccer(models.Model):
 
     def __str__(self):
         return self.title
-    
+
+class Comment(models.Model):
+    soccer = models.ForeignKey('blog.Soccer', related_name='comments')
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DataTimeField(default=timezone.now)
+    approved_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    def __str__(self):
+        return self.text
